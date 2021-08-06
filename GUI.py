9e6fd7d25aebdfe783f83c5b40cecb992c5f02e0
Den_K <den.k.work.01@gmail.com>
2021-08-06 11:12:13 +0300

    bigger font

--- GUI.py
+++ GUI.py
@@ -1,20 +1,28 @@
 import PySimpleGUI as sg
 
-
-
 # sg.theme('Dark Green 7')
 sg.theme('Topanga')      # Add some color to the window
+my_font = ("Consolas", 22)
+text_size=12
+inputsize=12
+button_size = 14
 
-text_size=8
-inputsize=15
-
-layout = [ 
-           [sg.Txt('Дата початок',size=(text_size,1)),sg.Input(key='-IN4-', size=(20,1)), sg.CalendarButton('Оберіть дату',  target='-IN4-', format = "%d-%m-%Y", )],
-           [sg.Txt('Дата кінець',size=(text_size,1)),sg.Input(key='-IN5-', size=(20,1)), sg.CalendarButton('Оберіть дату',  target='-IN5-', format = "%d-%m-%Y", )],
-           [sg.Button('Записи', bind_return_key=True),sg.Button('Exit') ]
+col1=[ 
+           [sg.Txt('Дата початок',size=(text_size,1),font=my_font),sg.Input(key='-IN4-', size=(inputsize,1),font=my_font)],
+           [sg.Txt('Дата кінець',size=(text_size,1),font=my_font),sg.Input(key='-IN5-', size=(inputsize,1),font=my_font)],           
+         ]
+col2=[ 
+           [sg.CalendarButton('Оберіть дату',  target='-IN4-', format = "%d-%m-%Y",font=my_font,size=(button_size,1) )],
+           [sg.CalendarButton('Оберіть дату',  target='-IN5-', format = "%d-%m-%Y",font=my_font,size=(button_size,1) )],
+           [sg.Button('Записи', bind_return_key=True,font=my_font,size=(button_size,1))],
+           [sg.Button('Exit',font=my_font,size=(button_size,1))]
          ]
 
-window = sg.Window('Записи часу', layout, grab_anywhere=False, size=(800, 480), no_titlebar=True, location=(0, 0), keep_on_top=True)
+layout = [ 
+           [sg.Column(col1),sg.Column(col2)],           
+         ]
+
+window = sg.Window('Записи часу', layout, grab_anywhere=False, size=(720, 480), no_titlebar=True, location=(0, 0), keep_on_top=True)
 
 while True:
     event, values = window.read()
