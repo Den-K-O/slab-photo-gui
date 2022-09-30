00d8361d4c4382dda5be84499a3eada6da4815af
Den_K <den.k.work.01@gmail.com>
2022-09-30 15:40:28 +0300

    testing

--- GUI.py
+++ GUI.py
@@ -1,5 +1,6 @@
 import PySimpleGUI as sg
 import base64
+import math
 import slab_photo_client_awaitable
 import asyncio
 from prnt_tspl import Printer,print_id
@@ -85,7 +86,7 @@ wood_species_lst = wood_species.copy()
 
 def wood_selection_window(wood_species):  
     MAX_COL = 3
-    MAX_ROWS = len(wood_species)//MAX_COL
+    MAX_ROWS = math.ceil(len(wood_species)/MAX_COL)
     #print (MAX_ROWS)
     layout =  [[sg.Button(wood_species_lst.pop(),font=my_font,size=(5,2),pad=((0,0),(0,0))) for j in range(MAX_COL)] for i in range(MAX_ROWS)]
 
